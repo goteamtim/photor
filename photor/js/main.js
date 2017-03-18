403fd7bb354f0abbffaf542ed79df600620e1e8e
Tim Harshman <goteamtim+git@gmail.com>
2017-03-18 09:38:56 -0700

    removed require

--- photor/js/main.js
+++ photor/js/main.js
@@ -1,13 +1,21 @@
 ﻿// Your code here!
-var Instafeed = require("https://raw.githubusercontent.com/stevenschobert/instafeed.js/master/instafeed.min.js");
+//var Instafeed = require("instafeed.min.js");
 
-var feed = new Instafeed({
-    get: 'tagged',
-    tagName: 'awesome',
-    clientId: ''
-});
-feed.run();
+// AMD
+//require(["instafeed.min.js"], function (Instafeed) {
+
+    var feed = new Instafeed({
+        get: 'tagged',
+        tagName: 'awesome',
+        clientId: ''
+    });
+    feed.run();
+    alert(feed)
 console.log(feed);
+
+//});
+
+
 
 window.onload = function () {
     document.getElementById("button").onclick = function (evt) {
