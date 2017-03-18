8d37ed6506b49dfb1124869b21f2399a36bce357
Tim Harshman <goteamtim+git@gmail.com>
2017-03-17 17:33:12 -0700

    Initial commit

--- photor/js/main.js
+++ photor/js/main.js
@@ -1,1 +1,22 @@
 ﻿// Your code here!
+var Instafeed = require("https://raw.githubusercontent.com/stevenschobert/instafeed.js/master/instafeed.min.js");
+
+var feed = new Instafeed({
+    get: 'tagged',
+    tagName: 'awesome',
+    clientId: ''
+});
+feed.run();
+console.log(feed);
+
+window.onload = function () {
+    document.getElementById("button").onclick = function (evt) {
+        sayHello()
+    }
+}
+
+
+function sayHello() {
+    var messageDialog = new Windows.UI.Popups.MessageDialog("Hello, world!", "Alert");
+    messageDialog.showAsync();
+}
