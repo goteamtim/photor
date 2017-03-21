b7079e29cb55864d82d76f8e3a79442230491545
Tim Harshman <goteamtim+git@gmail.com>
2017-03-21 07:36:02 -0700

    Try more stuff...

--- photor/js/main.js
+++ photor/js/main.js
@@ -4,18 +4,28 @@
 // AMD
 //require(["instafeed.min.js"], function (Instafeed) {
 
-    var feed = new Instafeed({
-        get: 'tagged',
-        tagName: 'awesome',
-        clientId: ''
-    });
-    feed.run();
-    alert(feed)
-console.log(feed);
+//    var feed = new Instafeed({
+//        get: 'tagged',
+//        tagName: 'awesome',
+//        clientId: ''
+//    });
+//    feed.run();
+//    alert(feed)
+//console.log(feed);
 
 //});
 
+// Initialize
+API = Instajam.init({
+    clientId: mine.clientID,
+    redirectUri: 'https://rudrastyh.com/tools/access-token',
+    scope: ['basic', 'comments']
+});
 
+// Get the profile of the authenticated user.
+API.user.self.profile(function (response) {
+    console.log('Hey there, ' + response.data.full_name);
+});
 
 window.onload = function () {
     document.getElementById("button").onclick = function (evt) {
@@ -27,4 +37,8 @@ window.onload = function () {
 function sayHello() {
     var messageDialog = new Windows.UI.Popups.MessageDialog("Hello, world!", "Alert");
     messageDialog.showAsync();
+}
+
+function displayWebSite() {
+    var wv = new Windows.UI.WebUI.
 }
